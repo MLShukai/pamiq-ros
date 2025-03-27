99ffc14d4e12c9679909ceb635d34613a6b78b08
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-03-27 07:14:18 +0000

    feat(pamiq_ros): expose environment class to top level module

--- src/pamiq_ros/__init__.py
+++ src/pamiq_ros/__init__.py
@@ -1,3 +1,11 @@
 from importlib import metadata
 
+from .env import CachedObsROS2Environment, ReactiveROS2Environment
+
 __version__ = metadata.version("pamiq-ros")
+
+
+__all__ = [
+    "CachedObsROS2Environment",
+    "ReactiveROS2Environment",
+]
